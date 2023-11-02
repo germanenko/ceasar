e9c71e533b754fd8d5feeda9b999a12966bbfe36
Даниил Пустовалов <pustden524@gmail.com>
2023-11-02 20:59:20 +0500

    выделение выбранного времени кружочком

--- Assets/[1] SOURCE/UI/Clock.cs
+++ Assets/[1] SOURCE/UI/Clock.cs
@@ -1,3 +1,4 @@
+using FlyingWormConsole3.LiteNetLib.Utils;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -12,10 +13,13 @@ public class Clock : MonoBehaviour
     [SerializeField] private float _hours;
     [SerializeField] private float _minutes;
 
+    [SerializeField] List<TimeLabel> _hourList;
+    [SerializeField] List<TimeLabel> _minuteList;
+
     void Start()
     {
-        //DrawHour(12, 100);
-        //DrawMinute(60, 185);
+        DrawHour(12, 120);
+        DrawMinute(60, 230);
     }
 
 
@@ -25,12 +29,29 @@ public class Clock : MonoBehaviour
         switch(type)
         {
             case TimeLabelType.Hour:
+                foreach(TimeLabel label in _hourList)
+                {
+                    if(label.TimeValue == time)
+                        label.SelectTime(true);
+                    else
+                        label.SelectTime(false);
+                }
                 _hours = time; 
                 break;
+
             case TimeLabelType.Minute:
+                foreach (TimeLabel label in _minuteList)
+                {
+                    if (label.TimeValue == time)
+                        label.SelectTime(true);
+                    else
+                        label.SelectTime(false);
+                }
                 _minutes = time;
                 break;
         }
+
+
 
         _timeText.text = string.Format("{0:00}:{1:00}", _hours, _minutes);
     }
@@ -51,12 +72,17 @@ public class Clock : MonoBehaviour
             float x = xScaled * radius;
             float y = yScaled * radius;
 
+
+
             var s = Instantiate(_timeLabel, transform);
 
             s.transform.localPosition = new Vector3(x, y, 0);
 
+            s.transform.localScale = s.transform.localScale * 1.5f;
+
             s.LabelType = TimeLabelType.Hour;
             s.Clock = this;
+
 
 
             if (i == 0)
@@ -69,6 +95,8 @@ public class Clock : MonoBehaviour
                 s.LabelText.text = i.ToString();
                 s.TimeValue = i;
             }
+
+            _hourList.Add(s);
         }
     }
 
@@ -88,17 +116,29 @@ public class Clock : MonoBehaviour
             float x = xScaled * radius;
             float y = yScaled * radius;
 
+
+
             var s = Instantiate(_timeLabel, transform);
 
             s.LabelType = TimeLabelType.Minute;
 
             s.transform.localPosition = new Vector3(x, y, 0);
 
-            s.LabelText.fontSize = 14;
-
-            s.LabelText.text = i.ToString();
             s.TimeValue = i;
             s.Clock = this;
+
+
+
+            if (i % 5 == 0)
+            {
+                s.LabelText.text = i.ToString();
+            }
+            else
+            {
+                s.LabelText.text = "";
+            }
+
+            _minuteList.Add(s);
         }
     }
 }
--- Assets/[1] SOURCE/UI/TimeLabel.cs
+++ Assets/[1] SOURCE/UI/TimeLabel.cs
@@ -20,8 +20,26 @@ public class TimeLabel : MonoBehaviour
 
     public Clock Clock;
 
+    public GameObject SelectIndicator;
+
     public void SetTime()
     {
         Clock.SetTime(TimeValue, LabelType);
+    }
+
+
+
+    public void SelectTime(bool select)
+    {
+        if (select)
+        {
+            SelectIndicator.SetActive(true);
+            LabelText.color = Color.white;
+        }
+        else
+        {
+            SelectIndicator.SetActive(false);
+            LabelText.color = Color.black;
+        }
     }
 }
